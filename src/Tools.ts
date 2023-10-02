a53e195c1fc130b5fe29df984b279d5416fd977f
Cooper Buch <cbuch609@gmail.com>
2023-10-02 11:59:37 -0500

    fixed bar charts

--- src/Tools.ts
+++ src/Tools.ts
@@ -1,27 +1,43 @@
 import { TankDataType } from "./Types";
-import * as Plotly from "plotly.js"
-
-
-
+import { Data, newPlot } from "plotly.js";
 
 function PlotData(data: TankDataType[]) {
-    const displayElement:HTMLDivElement = document.getElementById("graphDisplay") as HTMLDivElement
+  const displayElement: HTMLDivElement = document.getElementById(
+    "graphDisplay"
+  ) as HTMLDivElement;
 
+  const waterTrace: Data = {
+    x: [],
+    y: [],
+    name: "Water",
+    type: "bar",
+    marker:{
+      
+    }
+    
+  };
 
-    const plotData:Plotly.Data[] = [{
-        x:[1, 2, 7, 4],
-        y:["p1", "p2", "p3", "p4"],
-        name:"Top-Height",
-        type:"bar"
+  const oilTrace: Data = {
+    x: [],
+    y: [],
+    name: "Oil",
+    type: "bar",
+  };
 
-    }]
+  for (const tankdata of data) {
+    if (tankdata.Type == "WATER") {
+      (waterTrace.x as string[]).push(tankdata.Name);
+      (waterTrace.y as number[]).push(tankdata.LatestReading.top_feet);
+    }else if(tankdata.Type == "OIL"){
+      (oilTrace.x as string[]).push(tankdata.Name);
+      (oilTrace.y as number[]).push(tankdata.LatestReading.top_feet);
+    }
+  }
 
-
-    Plotly.newPlot(displayElement, plotData, {barmode: 'group'})
+  newPlot(displayElement, [waterTrace, oilTrace], { barmode: "group" });
 }
 
 async function GetWellData(wellName: string): Promise<TankDataType[]> {
-
   const res = await fetch(
     `/getWellTankReadings?wellName=${encodeURIComponent(wellName)}`
   );
@@ -30,5 +46,4 @@ async function GetWellData(wellName: string): Promise<TankDataType[]> {
   return wellTanks;
 }
 
-
-export {GetWellData, PlotData}
+export { GetWellData, PlotData };
