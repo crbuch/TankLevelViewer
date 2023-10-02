d87befdf8d2f5d0d45990b32d9c814e4ca07bca4
Cooper Buch <cbuch690@gmail.com>
2023-10-02 13:51:09 -0500

    Updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path");
-const webpack = require('webpack')
+const webpack = require("webpack");
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -10,12 +10,8 @@ const config = {
   output: {
     path: path.resolve(__dirname, "dist/static/scripts"),
   },
+
   plugins: [
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
-  plugins: [
-    // fix "process is not defined" error:
     new webpack.ProvidePlugin({
       process: "process/browser",
     }),
